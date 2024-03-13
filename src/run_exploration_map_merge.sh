b9eaa7decb21b034ba9dccc6a07f4964173a33a9
stephenmao0927 <939776651@qq.com>
2024-03-13 15:47:52 +0700

    adding a script to start exploration

---
+++ src/run_exploration_map_merge.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+# This script must run in docker.
+# Created and maintained by Stephen Mao (maok0002@e.ntu.edu.sg)
+
+
+source /opt/ros/melodic/setup.zsh
+source /home/user/catkin_ws/devel/setup.zsh
+
+rostopic pub /robot_1/ackermann_steering_controller/cmd_vel geometry_msgs/Twist "linear:
+  x: 1.0
+  y: 0.0
+  z: 0.0
+angular:
+  x: 0.0
+  y: 0.0
+  z: 0.0" -r 10 &
+ros_pub_pid1=$!
+
+rostopic pub /robot_2/ackermann_steering_controller/cmd_vel geometry_msgs/Twist "linear:
+  x: 1.0
+  y: 0.0
+  z: 0.0
+angular:
+  x: 0.0
+  y: 0.0
+  z: 0.0" -r 10 &
+ros_pub_pid2=$!
+
+sleep 5
+
+kill $ros_pub_pid1
+kill $ros_pub_pid2
+
+roslaunch multirobot_map_merge map_merge.launch  & roslaunch road_based_explorer multiple_test_traversability.launch
